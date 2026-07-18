0242ba0a5a430bb7ff2cf460e8c956b908601814
saching <saching@gamil.com>
2026-07-18 12:42:19 +0530

    added new files

---
+++ Register.java
@@ -0,0 +1,8 @@
+package remoterepo;
+
+public class Register {
+    public static void main(String[] args) {
+        System.out.println("this is register class");
+    }
+    
+}
---
+++ login.java
@@ -0,0 +1,8 @@
+package remoterepo;
+
+public class login {
+    public static void main(String[] args) {
+        System.err.println("this is main page");
+    }
+    
+}
